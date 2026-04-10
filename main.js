f1ba1f0e3d6814e27f41b69b946ff8105c1ac60f
Hybrid_Moments <zebraslon946@gmail.com>
2026-04-10 14:58:14 +0500

    Update main.js

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ import { testnetBradbury } from 'genlayer-js/chains';
 import { TransactionStatus } from 'genlayer-js/types';
 
 // ── CONSTANTS ──
-const CONTRACT_ADDRESS   = '0x9Ed920675fF174346505a2Eb795351c0A45cf187';
+const CONTRACT_ADDRESS   = '0x76b7B74A7a612985b12CFC2d2eE07C16E0Fa78f8';
 const GEN_PRICE          = BigInt('1000000000000000000'); // 1 GEN
 const REQUIRED_CHAIN_ID  = 4221;
 const REQUIRED_CHAIN_HEX = '0x107d';
